6247f5101a070059e6c1ad85d44c6cf1f15652b4
314lipczuk <3.14lipczuk@gmail.com>
2021-07-19 12:37:45 +0200

    lineparse progress and tri struct

--- stupidpars.cpp
+++ stupidpars.cpp
@@ -15,12 +15,28 @@ void parseLine(std::string &line, std::vector<float> &coords,std::vector<float> 
 
 struct tri{
 	float[3] norm;
-	float[3][3] points; 
-	bool is_viable_tri();
+	float** points; 
 	tri(float[3] norm);
 	~tri();
-
+  int num_points;
 };
+tri::tri(float[3] norm){
+  this->num_points=0;
+  this->norm = norm;
+  this->points = new float*[3];
+  for(int i=0;i<3;i++){
+    this->points[i] = new float[3];
+    for(int j=0;j<3,j++){
+      this->points[i][j]=0.0;
+    }
+  }
+}
+tri::~tri(){
+  for(int i=0;i<3;i++){
+    delete[] this->points[i];
+  }
+  delete[] this->points;
+}
 void parse_stl(std::string name, std::vector<float> coords,std::vector<float> normals,std::vector<int> tris,std::vector<int> solids)
 {
 	std::fstream my_file;
@@ -31,7 +47,7 @@ void parse_stl(std::string name, std::vector<float> coords,std::vector<float> no
 	if(isAscii(my_file)){
 		std::cout<< "Parsing ascii file" <<std::endl;
 		std::string line;
-		tri curtri;
+		//tri curtri;
 		while (std::getline(my_file, line)){
 			std::cout<<line<<std::endl;
 			parseLine(&line, &coords, &normals, &tris, &solids, &curtri);
@@ -42,18 +58,21 @@ void parse_stl(std::string name, std::vector<float> coords,std::vector<float> no
 	}
 }
 void parseLine(std::string &line,std::string &name, std::vector<float> &coords,std::vector<float> &normals,std::vector<int> &tris,std::vector<int> &solids ){
-	if( std::regex_match( line, std::regex("(*)(normal)(*)"))){
-		std::string pline = "";
-		std::cout<<"normal "<<line<<std::endl;
-		for(char& c:line){
-			if(std::isdigit(c) || std::isspace(c) ){
-				pline.append(c);
-			}
-		}
-		pline.split()
-		curtri = new triangle()
+  std::regexp("(-?[0-9]+\.[0-9]+)");
+  std::sregex_iterator next(line.begin(), line.end(), regexp);
+  std::sregex_iterator end;
+  float tmp[3]={};
+  int floatnum=0;
 
-	}
+	if( std::regex_match( line, std::regex("(*)(normal)(*)"))){
+    while(next!=end){
+      std::smatch match = *next;
+      tmp[floatnum] = std::stof(match.str());
+      next++;
+      floatnum++;
+	  }
+  
+  }
 	if( std::regex_match( line, std::regex("(*)(vertex)(*)"))){
 		std::cout<<""<<std::endl;	
 	}
