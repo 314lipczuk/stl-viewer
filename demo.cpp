14fb76794518da1c402547c4ca3c20fa105009a5
314lipczuk <3.14lipczuk@gmail.com>
2021-08-05 19:29:21 +0200

    no using whole namespaces in demo anymore

--- demo.cpp
+++ demo.cpp
@@ -1,27 +1,22 @@
 #include "Engine.h"
+#include <chrono>
 #include <cstdlib>
 #include <csignal>
 #include <ctime>
 int main(){
-    using namespace std::this_thread;
-    using namespace std::chrono_literals;
-    using std::chrono::system_clock;
-    using namespace Engine;
     Engine::buffer b(150,70);
-    loadStl("acube.stl");
-    centerShape();
-    Engine::scaleShape(3);
-    Engine::moveShape(0, 50, 50);
-    resizeForBuffer(b.offsety);
+    Engine::loadStl("ASCII.stl");
+    Engine::centerShape();
+    //Engine::scaleShape(3);
+    //Engine::moveShape(0, 50, 50);
+    Engine::resizeForBuffer(b.offsety);
     bool tooBigForLines = false;
     if(Engine::point::all_points.size()>500) tooBigForLines = true;
     for(;;){
       b.clear();
-
-      for(int x=0;x<line::all_lines.size();x++){
-          b.plotline(*line::all_lines[x]);
+      for(int x=0;x<Engine::line::all_lines.size();x++){
+          b.plotline(*Engine::line::all_lines[x]);
       }
-      
     if(tooBigForLines){
       for(auto itr = Engine::point::all_points.begin();itr != Engine::point::all_points.end();++itr){
           b.plot((*(*itr)));
@@ -32,7 +27,7 @@ int main(){
          (*itr)->rotate(1, 3.14/30);
          (*itr)->rotate(2, 3.14/40);
       }
-      sleep_for(150ms);
+      std::this_thread::sleep_for(std::chrono::milliseconds(150));
       Engine::clearScreen();
     }
 }
